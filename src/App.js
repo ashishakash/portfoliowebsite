198aa5828ac6a429ecab52fd6f3aa54bab9b8d36
Ashish Akash <ashishakash333@gmail.com>
2021-07-08 17:04:49 +0530

    tweaking around

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ function App() {
         <Navbar />
         <ScrollToTop />
         <Switch>
-          <Route path="/" component={Home} />
+          <Route exact path="/" component={Home} />
           <Route path="/about" component={About} />
           <Route path="/resume" component={Resume} />
         </Switch>
